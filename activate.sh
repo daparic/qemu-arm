03392fe948965abbeb461c325cce8e0c04554580
dx <dx@nuc>
2023-11-30 21:35:11 +0800

    Use posix portable to detect OSTYPE

--- activate.sh
+++ activate.sh
@@ -3,4 +3,13 @@
 # https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu.tar.xz
 ARM_CROSS_COMPILER=/d/compilers/gcc-linaro-7.5.0-2019.12-i686-mingw32_aarch64-linux-gnu
 
-export PATH=$ARM_CROSS_COMPILER/bin/:`pwd`/win:$PATH
+case "$(uname -s)" in
+    Linux*)
+    export PATH=$ARM_CROSS_COMPILER/bin/:$PATH
+    ;;
+    MINGW*|CYGWIN*)
+    export PATH=$ARM_CROSS_COMPILER/bin/:`pwd`/win:$PATH
+    ;;
+    *)
+    ;;
+esac
